8df6b1773422704bf6c1ab0868f1bbfdecf10d2e
Darkwa <pierre.duros@hotmail.fr>
2017-04-20 11:11:48 +0200

    Created linux script to compile and run program. Removed unused libs.

---
+++ src/run.sh
@@ -0,0 +1,2 @@
+javac -cp .:../lib/json-20080701.jar:../lib/okhttp-3.6.0.jar:../lib/okio-1.11.0.jar resources/enums/Actions.java  resources/enums/Characters.java resources/enums/GameStatus.java resources/effectResources/Effect.java  resources/actionResources/Action.java resources/charactersResources/Character.java resources/RestResources/RestRequests.java resources/playerResources/Player.java resources/gameResources/Game.java main/main.java
+java -cp .:../lib/json-20080701.jar:../lib/okhttp-3.6.0.jar:../lib/okio-1.11.0.jar main.main
